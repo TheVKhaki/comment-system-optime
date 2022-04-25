43606c4d619074d643edfe33e017d46ad571769d
TheVKhaki <TheVKhaki@gmail.com>
2022-04-25 14:06:27 +0430

    Login dashboard + responsive

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,17 +2,22 @@ import "./styles/app.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "swiper/css";
 import "swiper/css/pagination";
+//Page
 import Home from "./page/Home";
+import Login from "./page/Login";
+//Components
 import Header from "./components/Header";
 import Footer from "./components/Footer";
-import { Route, Routes } from "react-router-dom";
 import SocialSection from "./components/Social";
+//Router
+import { Route, Routes } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <Header />
-      <Home />
+      <Login />
+      {/* <Header />
+      <Home /> */}
       {/* <Footer /> */}
     </div>
   );
