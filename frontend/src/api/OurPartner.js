8559eaa9283ddcbbe642a6cb11bd38cd537a2923
Mohammadreza MousaviNiya <mohammadrezamousaviniya@gmail.com>
2022-06-25 22:58:38 +0430

    Minor improvement 2

--- frontend/src/api/OurPartner.js
+++ frontend/src/api/OurPartner.js
@@ -3,6 +3,11 @@ import bscscanLogo from "../images/bscscan-logo.svg";
 import openseaLogo from "../images/OpenSea-Full-Logo (light).svg";
 import polygonLogo from "../images/polygon_token_logo-freelogovectors.net_.svg";
 import quickswapLogo from "../images/QuickSwap-Exchange-Logo.png";
+
+import alchemy from "../images/alchemy.png";
+import openzepplin from "../images/oppen_Zepplin.webp";
+import certik from "../images/certik.svg";
+
 const OurPartnerApi = () => {
   return [
     {
@@ -25,6 +30,18 @@ const OurPartnerApi = () => {
       id: 5,
       imgSrc: binanceLogo,
     },
+    {
+      id: 6,
+      imgSrc: alchemy,
+    },
+    {
+      id: 7,
+      imgSrc: openzepplin,
+    },
+    {
+      id: 8,
+      imgSrc: certik,
+    }
   ];
 };
 
